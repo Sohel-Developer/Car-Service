32e3b1dea9aab94ca76b080e3a075a1b63cad8d5
Sohel-Developer <sohel.webdevoloper.com>
2022-11-07 19:41:33 +0600

    some pages create and router setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import { RouterProvider } from 'react-router-dom';
 import './App.css';
+import { router } from './Router/Routes/Routes';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Ready Work </h1>
+    <div>
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
